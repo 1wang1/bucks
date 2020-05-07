e82ab973b1f6444bf7cb917a8ee983b91348c59e
wangzhenkun <wangzhenkun@ruijie.com.cn>
2020-05-07 21:57:15 +0800

    redisのjedis的简单应用

--- src/main/java/com/wzk/bucks/BucksApplication.java
+++ src/main/java/com/wzk/bucks/BucksApplication.java
@@ -7,14 +7,24 @@ import com.wzk.bucks.repository.CoffeeRepository;
 import com.wzk.bucks.service.CoffeeOrderService;
 import com.wzk.bucks.service.CoffeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -22,25 +32,56 @@ import java.util.Optional;
 @EnableJpaRepositories
 @SpringBootApplication
 public class BucksApplication implements ApplicationRunner {
-	@Autowired
-	private CoffeeRepository coffeeRepository;
-	@Autowired
-	private CoffeeService coffeeService;
-	@Autowired
-	private CoffeeOrderService orderService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BucksApplication.class, args);
-	}
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+    @Autowired
+    private CoffeeService coffeeService;
+    @Autowired
+    private CoffeeOrderService orderService;
+    @Autowired
+    private JedisPool jedisPool;
+    @Autowired
+    private JedisPoolConfig jedisPoolConfig;
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		log.info("All Coffee: {}", coffeeRepository.findAll());
-		Optional<Coffee> latte = coffeeService.findOneCoffee("Latte");
-		if (latte.isPresent()) {
-			CoffeeOrder order = orderService.createOrder("Li Lei", latte.get());
-			log.info("Update INIT to PAID: {}", orderService.updateState(order, OrderState.PAID));
-			log.info("Update PAID to INIT: {}", orderService.updateState(order, OrderState.INIT));
-		}
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BucksApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info(jedisPoolConfig.toString());
+        try (Jedis jedis = jedisPool.getResource()) {
+            List<Coffee> allCoffee = coffeeService.findAllCoffee();
+            log.info("all coffee:{}",allCoffee.size());
+            log.info("all coffee:{}",allCoffee);
+            allCoffee.forEach(c -> {
+                jedis.hset("springbucks-menu", c.getName(),
+                           Long.toString(c.getPrice().getAmountMinorLong()));
+            });
+            Map<String, String> menu = jedis.hgetAll("springbucks-menu");
+            log.info("Menu;{}", menu);
+
+            String price = jedis.hget("springbucks-menu", "espresso");
+            log.info("espresson-{}", Money.ofMinor(CurrencyUnit.of("CNY"), Long.parseLong(price)));
+        }
+//        log.info("All Coffee: {}", coffeeRepository.findAll());
+//        Optional<Coffee> latte = coffeeService.findOneCoffee("Latte");
+//        if (latte.isPresent()) {
+//            CoffeeOrder order = orderService.createOrder("Li Lei", latte.get());
+//            log.info("Update INIT to PAID: {}", orderService.updateState(order, OrderState.PAID));
+//            log.info("Update PAID to INIT: {}", orderService.updateState(order, OrderState.INIT));
+//        }
+    }
+
+    @Bean
+    @ConfigurationProperties("redis")
+    public JedisPoolConfig jedisPoolConfig() {
+        return new JedisPoolConfig();
+    }
+
+    @Bean(destroyMethod = "close")
+    public JedisPool jedisPool(@Value("${redis.host}") String host) {
+        return new JedisPool(jedisPoolConfig(), host);
+    }
 }
--- src/main/java/com/wzk/bucks/service/CoffeeService.java
+++ src/main/java/com/wzk/bucks/service/CoffeeService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -27,5 +28,9 @@ public class CoffeeService {
         Optional<Coffee> coffee = coffeeRepository.findOne(example);
         log.info("Coffee Found: {}", coffee);
         return coffee;
+    }
+
+    public List<Coffee> findAllCoffee() {
+        return coffeeRepository.findAll();
     }
 }
